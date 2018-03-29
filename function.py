03f5d074ddb5f94906ea58f9379bab9dd764274b
minglangqiao <463034548@qq.com>
2018-03-29 20:37:02 +0800

    0329 routing

--- function.py
+++ function.py
@@ -1,6 +1,9 @@
 import numpy as np
 import os
 from video_subjects import *
+import json
+import imageio
+
 
 
 def get_dir_files(file_path):
@@ -11,4 +14,129 @@ def get_dir_files(file_path):
         break
 
     return  f
+
+
+def get_all_video_config():
+
+    for i_video in range(len(video_list)):
+        video_name = video_list[i_video]
+        video_path = ('./all_video/' + video_name + '/video/' +
+                      video_name + '.mp4')
+
+        FRAMERATE, FRAMESCOUNT, IMAGEWIDTH, IMAGEHEIGHT = get_video_config(
+            video_path)
+
+
+        save_txt(video_name, FRAMESCOUNT, IMAGEWIDTH, IMAGEHEIGHT)
+
+        print("video: %d"%i_video)
+
+
+def read_all_video_config():
+
+    video_config = []
+
+    path = 'Data/Video_config/'
+    read_path =  path + '0329_video_config.txt'
+    f = open(read_path, 'r')
+    lines = f.readlines()
+
+    for line in lines:
+        line = line.split()
+        video_config.append([line[0], line[1], line[2], line[3]])
+
+    return video_config
+
+def get_one_video_fixations():
+
+    pass
+
+
+def get_all_video_fxiation():
+    pass
+
+
+def get_video_config(video_path):
+    file_in_1 = video_path
+    # # get the paramters
+    # # get the paramters
+    video = imageio.get_reader(file_in_1, 'ffmpeg')
+
+    FRAMERATE= round(video._meta['fps'])
+    FRAMESCOUNT = video._meta['nframes']
+    Frame_size  = video._meta['source_size']
+    IMAGEWIDTH = round(Frame_size[0])
+    IMAGEHEIGHT = round(Frame_size[1])
+    Second_total = FRAMESCOUNT / FRAMERATE
+
+    return FRAMERATE,  FRAMESCOUNT,  IMAGEWIDTH, IMAGEHEIGHT
+
+
+def save_txt(video_name, FRAMESCOUNT, IMAGEWIDTH, IMAGEHEIGHT):
+
+    path = 'Data/Video_config/'
+
+    save_path =  path + '0329_video_config.txt'
+    save_data = ( str(video_name) + '\t' + str(FRAMESCOUNT) + '\t'  +
+                  str(IMAGEWIDTH)  + '\t' + str(IMAGEHEIGHT) + '\n' )
+
+    f = open(save_path, 'a')
+    f.write(save_data)
+    f.close()
+
+def extract_fixations(video_name):
+    """
+    extract one subject's fixation data
+
+    :parameter
+    video_name：
+
+
+    :return:
+    subject_num: the number of subject in one video
+    fixations: a m x n list, where m is the subject_num, n is the fiaxtion data
+               length one video's fixations
+
+    """
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import numpy as np
 from config import *
 from function import *
+import time
 
 
 def main():
 
-    all_videos = get_dir_files(file_path = video_path)
-    for i in all_videos:
-        print("'%s',"%i)
+    # all_videos = get_dir_files(file_path = video_path)
+    # get_all_video_config()
+    video_config = read_all_video_config()
+
+
+
 
 
 
