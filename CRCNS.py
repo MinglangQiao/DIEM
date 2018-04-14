5e09569e03aae2b2c2b1e3a53e9858937956fe5a
minglangqiao <minglangqiao@buaa.edu.cn>
2018-04-14 17:06:04 +0800

    finish CRCNS

--- CRCNS.py
+++ CRCNS.py
@@ -1,29 +1,149 @@
 from config import *
+from function import *
+
+def get_total_frame(path = CRCNS_frame_path):
+    all_frame = 0
+    all_data = []
+    f = open(path)
+    lines = f.readlines()
+    for line in lines:
+        line = line.split()
+        all_data.append(line)
+
+    for i_video in range(1, 51):
+        all_frame += float(all_data[i_video][1])
+        print(all_data[i_video][1])
+
+    all_frame = all_frame - CRCNS_trash_frame * CRCNS_video_num
+    print(">>>> total_valid_frame: %d"%all_frame)
+    a = 1
+
+def get_video_frame(video_name, path = CRCNS_frame_path):
+    all_frame = 0
+    all_data = []
+    f = open(path)
+    lines = f.readlines()
+    for line in lines:
+        line = line.split()
+        all_data.append(line)
+
+    for i_video in range(1, 51):
+
+        if video_name == all_data[i_video][0][:-4]:
+            frame_num = all_data[i_video][1]
+
+    return int(frame_num)
+
+def get_video_config(video_name, video_path = CRCNS_video_path):
+
+    file_in_1 = video_path + video_name + '.mpg'
+    # # get the paramters
+    # # get the paramters
+    video = imageio.get_reader(file_in_1, 'ffmpeg')
+
+    FRAMERATE= round(video._meta['fps'])
+    FRAMESCOUNT = video._meta['nframes']
+    Frame_size  = video._meta['source_size']
+    IMAGEWIDTH = round(Frame_size[0])
+    IMAGEHEIGHT = round(Frame_size[1])
+    Second_total = FRAMESCOUNT / FRAMERATE
+
+    return FRAMERATE,  FRAMESCOUNT,  IMAGEWIDTH, IMAGEHEIGHT
 
 
-def get_total_frame():
-    pass
+def get_subject_list(path = CRCNS_data_path):
+    f =  get_dirs(path)
+    for i in f:
+        print("'%s',"%i)
 
 
+def get_video_list(path = CRCNS_frame_path):
+    all_frame = 0
+    all_data = []
+    f = open(path)
+    lines = f.readlines()
+    for line in lines:
+        line = line.split()
+        all_data.append(line)
 
+    for i_video in range(1, 51):
+        video_name = all_data[i_video][0]
+        print("'%s',"%video_name)
 
+    a = 1
 
+def get_all_valid_fixation(path = CRCNS_data_path):
 
+    all_data = []
+    all_fixation_num = 0
+    for i_subject in range(len(CRCNS_subjects)):
+    # for i_subject in range(7, 8):
+        subject_name = CRCNS_subjects[i_subject]
 
+        for i_video in range(len(CRCNS_video_list)):
+        # for i_video in range(0, 4):
 
+            # !!!!!!!!!!!!!!!! remember to clear each time
+            all_data = []
+            video_name = CRCNS_video_list[i_video][:-4]
+            video_frame = get_video_frame(video_name)
 
+            try:
+                data_path = (path + '/' + subject_name + '/' +
+                             video_name + '.e-ceyeS')
 
+                f = open(data_path, 'r')
+                lines = f.readlines()
+                for line in lines:
+                    line = line.split()
+                    all_data.append(line)
+                ## !!! remenber to close each time
+                f.close()
 
+                record_eye_num = len(all_data) - 3
+                record_each_frame =  record_eye_num/video_frame
 
+                for i_frame in range(271, video_frame):
+
+                    frame_start =  int(i_frame * record_each_frame) + 3
+                    frame_end =  int((i_frame+1) * record_each_frame) + 3
+                    # print("===i_subject: %d, i_video: %d, frame_start: %d, "
+                    #       "frame_end: %d, record_eye_num: %d"%(i_subject,
+                    #     i_video, frame_start, frame_end, record_eye_num))
+                    if frame_end >= record_eye_num:
+                        frame_end = record_eye_num  - 1
+
+                    for i_fixation_frame in range(frame_start, frame_end):
+                        if float(all_data[i_fixation_frame][3]) == 0:
+                            all_fixation_num += 1
+                            break
+                        else:
+                            pass
+                            # print(">>> debug:", all_data[i_fixation_frame][3])
+
+                    # satart_frame = CRCNS_trash_frame + 3
+                    # fixation_each_frame =
+
+                print("video_frame: %d, i_video: %d, record_eye_num: "
+                      "%d, frame_start: %d, frame_end: %d"%
+                      (video_frame, i_video, record_eye_num, frame_start, frame_end))
+            except Exception as e:
+                print(">>>>>>>> file error: %s, i_video: %d, record_eye_num: "
+                      "%d, frame_start: %d, frame_end: %d"%
+                      (e, i_video, record_eye_num, frame_start, frame_end))
+
+        print('================================ i_subject: %d i_video: %s, fix_num: %d' % (
+            i_subject, i_video, all_fixation_num))
 
 
 
 def main():
-    pass
 
-
-
-
+    # get_total_frame()
+    get_all_valid_fixation()
+    # for i_video in range(len(CRCNS_video_list)):
+    #     video_name = CRCNS_video_list[i_video][:-4]
+    #     get_all_valid_fixation(video_name, path=CRCNS_frame_path)
 
 
 
@@ -32,4 +152,4 @@ def main():
 
 
 if __name__ == "__main__":
-    mai()
+    main()
--- config.py
+++ config.py
@@ -26,9 +26,84 @@ pair_end = SET_Value
 Finding_N_subjects = 9
 
 ############################ for CRCNS ########################################
-CRCNS_path = "数据库_data/CRCNS/crcns-eye1-1/CRCNS-DataShare/"
+CRCNS_path = "数据库_data/CRCNS/crcns-eye1-1/"
 
-frame_path = CRCNS_path + "MPGSIZES.txt"
+CRCNS_video_path = "数据库_data/CRCNS/crcns-eye1-1/stimuli/"
+CRCNS_frame_path = CRCNS_path + "MPGSIZES.txt"
+CRCNS_data_path = "数据库_data/CRCNS/crcns-eye1-2/data-orig"
+
 # see the README.txt for detail
-CRCNS_start_frame = 271
+CRCNS_trash_frame = 270
+CRCNS_video_num = 50
+CRCNS_valid_frame = 32989
+CRCNS_valid_fixation = 118785
+CRCNS_ave_fixation = 3.60
+
+
+
+CRCNS_video_list = [
+'beverly01.mpg',
+'beverly03.mpg',
+'beverly05.mpg',
+'beverly06.mpg',
+'beverly07.mpg',
+'beverly08.mpg',
+'gamecube02.mpg',
+'gamecube04.mpg',
+'gamecube05.mpg',
+'gamecube06.mpg',
+'gamecube13.mpg',
+'gamecube16.mpg',
+'gamecube17.mpg',
+'gamecube18.mpg',
+'gamecube23.mpg',
+'monica03.mpg',
+'monica04.mpg',
+'monica05.mpg',
+'monica06.mpg',
+'saccadetest.mpg',
+'standard01.mpg',
+'standard02.mpg',
+'standard03.mpg',
+'standard04.mpg',
+'standard05.mpg',
+'standard06.mpg',
+'standard07.mpg',
+'tv-action01.mpg',
+'tv-ads01.mpg',
+'tv-ads02.mpg',
+'tv-ads03.mpg',
+'tv-ads04.mpg',
+'tv-announce01.mpg',
+'tv-music01.mpg',
+'tv-news01.mpg',
+'tv-news02.mpg',
+'tv-news03.mpg',
+'tv-news04.mpg',
+'tv-news05.mpg',
+'tv-news06.mpg',
+'tv-news09.mpg',
+'tv-sports01.mpg',
+'tv-sports02.mpg',
+'tv-sports03.mpg',
+'tv-sports04.mpg',
+'tv-sports05.mpg',
+'tv-talk01.mpg',
+'tv-talk03.mpg',
+'tv-talk04.mpg',
+'tv-talk05.mpg'
+]
+
+CRCNS_subjects = [
+    'VN',
+    'ND',
+    'JZ',
+    'NM',
+    'JA',
+    'CZ',
+    'VC',
+    'RC'
+]
+
+
 
